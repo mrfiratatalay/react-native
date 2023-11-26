63fb0f204538b1948914e9759b1aa01c05890006
Firat Atalay <atafiratbey@gmail.com>
2023-11-26 23:53:53 +0300

    Start to learn React

---
+++ 00/App.js
@@ -0,0 +1,16 @@
+import { Text } from "react-native";
+import { Profil } from "./components/Profil/Profil";
+export default function App() {
+  return (
+    <>
+      <Text>FIRAT ATALAY</Text>
+      <Text>FIRAT</Text>
+      <Text>FIRAT ATALAY</Text>
+      <Text>FIRAT ATALAY</Text>
+      <Text>FIRAT ATALAY</Text>
+      <Text>FIRAT ATALAY</Text>
+      <Profil></Profil>
+      <Profil />
+    </>
+  );
+}
---
+++ 00/components/Profil/Profil.jsx
@@ -0,0 +1,13 @@
+import { TouchableOpacity, Text } from "react-native";
+
+export function Profil() {
+  return (
+    <>
+      <Text>IF YOU WANT TO SEE FIRAT</Text>
+
+      <TouchableOpacity>
+        <Text>CLICK ME</Text>
+      </TouchableOpacity>
+    </>
+  );
+}
