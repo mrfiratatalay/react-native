0200ba10fa6a4b759d892d9227037318b407b47f
Firat Atalay <atafiratbey@gmail.com>
2023-12-03 21:38:19 +0300

    To Learn React Native...

---
+++ 05/01/App.js
@@ -0,0 +1,32 @@
+import { Text, View, Image } from "react-native";
+import { ProfilCard } from "./components/ProfilCard/ProfilCard";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet } from "react-native";
+
+import { s } from "./components/ProfilCard/ProfilCard.style";
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1, justifyContent: "center", padding: 20 }}>
+        <ProfilCard
+          firstName={"Anjelia"}
+          lastName={"Hola"}
+          age={13}
+          isOpentoWork={true}
+          car={{ brand: "Seat", maxSpeed: 700 }}
+          doSomething={function () {
+            return "Welcome to learn props !!";
+          }}
+          img={
+            <Image
+              style={{ height: 30, width: 30 }}
+              source={{ uri: "https://i.pravatar.cc/300" }}
+            />
+          }
+        >
+          Hi!!!
+        </ProfilCard>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
